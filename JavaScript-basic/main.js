ed90127f15c068bad00c2677a3b65ee5499dd739
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-10 20:28:55 +0700

    first commit

---
+++ JavaScript-basic/main.js
@@ -0,0 +1,43 @@
+/*
+// dùng để khai báo biến
+// var fullname = 'Đào Ngọc Linh';
+// var age = 21;
+// gọi hàm thông báo
+alert(fullname);
+alert(age);
+*/
+
+// prompt('Xác nhận bạn đủ tuổi');
+// alert('cảm ơn bạn đã xác nhận');
+
+// setTimeout(function(){
+// alert('Xin lỗi vì đã để bạn phải đợi');
+// },10000);
+
+// setInterval(function(){
+//  console.log('Đây là ảo ma' + Math.random());
+// },2000)
+
+
+//Toán tử trong Js
+
+// var a = 1+2;
+// console.log(a);
+//toán tử số học
+
+// var fullname = 'Đào ngọc linh';
+//toán tử gán
+
+// var a= 1;
+// var b = 1;
+//  if(a == b) {
+//      alert('đúng');
+//  };
+//toán tử so sánh
+
+// var a = 1;
+// var b = 2;
+// if(a>0 && b>0){
+//     alert('a,b đều lớn hơn k')
+// }
+//Toán tử logic
