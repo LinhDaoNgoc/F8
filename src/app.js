a6ddc360094fc0b09cfe717451784eb081b3aefc
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-21 15:38:33 +0700

    feat: update

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    // var a = 1;
+    // var b = 2;
+
+    // var c = a + b;
+    // console.log(c);
+
+    return res.send('Chào bạn Linh nhé <3')
 })
 
 app.listen(port, () => {
